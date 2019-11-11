f549eb488cf4e89e517051d7298b18fc4754f855
Ross McLane <ross.mclane@gmail.com>
2019-11-11 17:44:05 -0500

    Adding basic axios functionality for omdb and seatgeek

--- keys.js
+++ keys.js
@@ -4,3 +4,12 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+
+exports.seatgeek = {
+  id: process.env.SEATGEEK_ID
+};
+
+exports.omdb = {
+  key: process.env.OMDB_KEY
+};
--- liri.js
+++ liri.js
@@ -2,24 +2,53 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
+var axios = require('axios');
 
+// Get Arguments
+var operation = process.argv[2];
+var query_arg = process.argv[3];
 
-var spotify = new Spotify(keys.spotify);
+if (operation === "spotify-this-song") {
 
-spotify.search({ type: 'track', query: 'Hello' }, function (err, data) {
-    if (err) {
-        return console.log('Error occurred: ' + err);
-    }
+    // var spotify = new Spotify(keys.spotify);
 
-    // Artists
-    console.log(data.tracks.items[0].artists[0].name);
+    // spotify.search({ type: 'track', query: query_arg }, function (err, data) {
+    //     if (err) {
+    //         return console.log('Error occurred: ' + err);
+    //     }
 
-    // Song Name
-    console.log(data.tracks.items[0].name);
+    //     // Artists
+    //     console.log(data.tracks.items[0].artists[0].name);
 
-    // Album Name
-    console.log(data.tracks.items[0].album.name);
+    //     // Song Name
+    //     console.log(data.tracks.items[0].name);
 
-    // link
-    console.log(data.tracks.items[0].external_urls.spotify);
-});
+    //     // Album Name
+    //     console.log(data.tracks.items[0].album.name);
+
+    //     // link
+    //     console.log(data.tracks.items[0].external_urls.spotify);
+    // });
+
+} else if (operation === 'concert-this') {
+
+    // axios({
+    //     method: 'get',
+    //     url: `https://api.seatgeek.com/2/events?client_id=${keys.seatgeek.id}`
+    // })
+    //     .then(function (response) {
+    //         console.log(response.data.events[0].title);
+    //     })
+} else if (operation === 'movie-this') {
+
+    console.log(keys.omdb.key);
+
+    axios({
+        method: 'get',
+        url: `https://www.omdbapi.com/?t=${query_arg}&apikey=${keys.omdb.key}`
+    })
+        .then(function (response) {
+            console.log(response.data.Title);
+        })
+
+};
