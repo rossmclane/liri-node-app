0e8535de6f9d9f4257152a96a5d1419ec21a951b
Ross McLane <ross.mclane@gmail.com>
2019-11-12 14:28:49 -0500

    Adding the do-what-it-says functionality, plus allowing for multiple query search words

--- liri.js
+++ liri.js
@@ -3,56 +3,93 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var Spotify = require('node-spotify-api');
 var axios = require('axios');
+var fs = require('fs');
 
 // Get Arguments
 var operation = process.argv[2];
-var query_arg = process.argv[3];
+var query_arg = process.argv.slice(3).join("+");
 
-if (operation === "spotify-this-song") {
+function run_liri(operation, query_arg) {
 
-    // var spotify = new Spotify(keys.spotify);
+    // Branching conditions
+    if (operation === "spotify-this-song") {
+        querySpotify(query_arg);
+    } else if (operation === 'concert-this') {
+        querySeatgeek(query_arg);
+    } else if (operation === 'movie-this') {
+        queryOMDB(query_arg);
+    } else if (operation === 'do-what-it-says') {
 
-    // spotify.search({ type: 'track', query: query_arg }, function (err, data) {
-    //     if (err) {
-    //         return console.log('Error occurred: ' + err);
-    //     }
+        console.log(operation);
 
-    //     // Artists
-    //     console.log(data.tracks.items[0].artists[0].name);
+        fs.readFile('./random.txt', 'utf-8', function (err, data) {
+            line = data.replace(/\n|"/g, "").split(',');
+            operation = line[0];
+            query_arg = line[1].split(' ').join('+');
 
-    //     // Song Name
-    //     console.log(data.tracks.items[0].name);
+            run_liri(operation, query_arg);
 
-    //     // Album Name
-    //     console.log(data.tracks.items[0].album.name);
+        });
+    }
+}
 
-    //     // link
-    //     console.log(data.tracks.items[0].external_urls.spotify);
-    // });
+run_liri(operation, query_arg);
 
-} else if (operation === 'concert-this') {
+function querySpotify(query_arg) {
+    var spotify = new Spotify(keys.spotify);
 
+    spotify.search({ type: 'track', query: query_arg }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        // Artist
+        console.log(data.tracks.items[0].artists[0].name);
+
+        // Song Name
+        console.log(data.tracks.items[0].name);
+
+        // Album Name
+        console.log(data.tracks.items[0].album.name);
+
+        // link
+        console.log(data.tracks.items[0].external_urls.spotify);
+    });
+}
+
+function querySeatgeek(query_arg) {
     axios({
         method: 'get',
         url: `https://api.seatgeek.com/2/events?q=${query_arg}&client_id=${keys.seatgeek.id}`
     })
         .then(function (response) {
             console.log(response.data.events[0].title);
+            // Print this in a prettier format
+            console.log(response.data.events[0].datetime_local);
+            console.log(response.data.events[0].venue.name + response.data.events[0].venue.display_location);
         })
-} else if (operation === 'movie-this') {
+}
+
+function queryOMDB(query_arg) {
+    if (query_arg === "") {
+        query_arg = "Mr. Nobody"
+    }
 
     axios({
         method: 'get',
         url: `https://www.omdbapi.com/?t=${query_arg}&apikey=${keys.omdb.key}`
     })
         .then(function (response) {
+            console.log(response.data.Ratings);
             console.log('*' + response.data.Title);
             console.log('*' + response.data.Year);
             console.log('*' + response.data.imdbRating);
-            console.log('*' + response.data.Ratings[1].Value);
+            try {
+                console.log('*' + response.data.Ratings[1].Value);
+            } catch {
+                console.log('*No Rotten Tomatoes Rating');
+            }
             console.log('*' + response.data.Country);
             console.log('*' + response.data.Plot);
             console.log('*' + response.data.Actors);
         })
-
-};
+}
